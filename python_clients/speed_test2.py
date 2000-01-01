fd09d65209703a799ab6dd483fd64a574fce9db9
David Wallace <david.wallace@gmail.com>
2000-01-01 22:13:56 +0000

    turn on profiling in speed_test2.py

--- python_clients/speed_test2.py
+++ python_clients/speed_test2.py
@@ -68,38 +68,52 @@ print
 #-------------------------------------------------------------------------------
 # send pixels
 
-print '    sending pixels forever (control-c to exit)...'
-print
+def main():
+    print '    sending pixels forever (control-c to exit)...'
+    print
 
-# how many sine wave cycles are squeezed into our n_pixels
-# 24 happens to create nice diagonal stripes on the wall layout
-freq_r = 24
-freq_g = 24
-freq_b = 24
+    # how many sine wave cycles are squeezed into our n_pixels
+    # 24 happens to create nice diagonal stripes on the wall layout
+    freq_r = 24
+    freq_g = 24
+    freq_b = 24
 
-# how many seconds the color sine waves take to shift through a complete cycle
-speed_r = 7
-speed_g = -13
-speed_b = 19
+    # how many seconds the color sine waves take to shift through a complete cycle
+    speed_r = 7
+    speed_g = -13
+    speed_b = 19
 
-start_time = time.time()
-while True:
-    t = time.time() - start_time
+    start_time = time.time()
     pixels = []
     for ii in range(n_pixels):
-        pct = ii / n_pixels
-        r = pct * 256
-        g = pct * 256
-        b = pct * 256
-#         # diagonal black stripes
-#         pct_jittered = (pct * 77) % 37
-#         blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
-#         blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
-#         blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
-#         # 3 sine waves for r, g, b which are out of sync with each other
-#         r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
-#         g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
-#         b = blackstripes * color_utils.remap(math.cos((t/speed_b + pct*freq_b)*math.pi*2), -1, 1, 0, 256)
-        pixels.append((r, g, b))
-    client.put_pixels(pixels, channel=0)
+        pixels.append([0,0,0])
+
+#     while True:
+    for ii in xrange(1000):
+        t = time.time() - start_time
+        for ii in range(n_pixels):
+            pct = ii / n_pixels
+            r = pct * 256
+            g = pct * 256
+            b = pct * 256
+    #         # diagonal black stripes
+    #         pct_jittered = (pct * 77) % 37
+    #         blackstripes = color_utils.cos(pct_jittered, offset=t*0.05, period=1, minn=-1.5, maxx=1.5)
+    #         blackstripes_offset = color_utils.cos(t, offset=0.9, period=60, minn=-0.5, maxx=3)
+    #         blackstripes = color_utils.clamp(blackstripes + blackstripes_offset, 0, 1)
+    #         # 3 sine waves for r, g, b which are out of sync with each other
+    #         r = blackstripes * color_utils.remap(math.cos((t/speed_r + pct*freq_r)*math.pi*2), -1, 1, 0, 256)
+    #         g = blackstripes * color_utils.remap(math.cos((t/speed_g + pct*freq_g)*math.pi*2), -1, 1, 0, 256)
+    #         b = blackstripes * color_utils.remap(math.cos((t/speed_b + pct*freq_b)*math.pi*2), -1, 1, 0, 256)
+            pixel = pixels[ii]
+            pixel[0] = r
+            pixel[1] = g
+            pixel[2] = b
+        client.put_pixels(pixels, channel=0)
+
+print '-------------- profiler off'
+main()
+print '-------------- profiler on'
+import cProfile
+cProfile.run('main()')
 
